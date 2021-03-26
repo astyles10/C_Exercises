8719e404d7a2dae20f46d160651667bf7ad8b370
astyles <astyles@ats-t1.com>
2021-03-26 16:20:37 +1100

    Update tail code

--- Chapter5/tail.c
+++ Chapter5/tail.c
@@ -1,32 +1,111 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXLINE 100
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#define MAXLINE 1000
 
+int printError(char *err);
 int fgetline(char *line, int max);
-void appendLine (char *lines[], char *line);
+int appendLine(char *lines[], char *line);
 void copy(char to[], char from[]);
 int counter = 0;
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char line[MAXLINE];
     char *lines[MAXLINE];
     int len = 0;
-    // Get lines from stdin
-    while (len = fgetline(line, MAXLINE) > 1) {
-        appendLine(lines, line);
+    int numLines = 0;
+    char errString[MAXLINE] = "Invalid option: \'";
+
+    if (argc >= 2)
+    {
+        int lim = argc - 1;
+
+        // Get options, increment i for each option
+        int i = 1;
+        if (argv[i][0] == '-') {
+            int opt = argv[i][1];
+            int optItr = 2;
+            switch(opt) {
+                case 'n':
+                    // Check if anything follows -n
+                    if (strlen(argv[i]) < 3) {
+                        strcat(errString, argv[i]);
+                        strcat(errString, "\' - Number of lines required");
+                        return printError(errString);
+                    }
+                    char* numPtr = &argv[i][optItr];
+
+                    // Check if following input is a number
+                    for (int x = 0; x < strlen(numPtr); x++) {
+                        if (!isdigit(numPtr[x])) {
+                            strcat(errString, argv[i]);
+                            strcat(errString, "\' - Invalid number of lines: ");
+                            strcat(errString, numPtr);
+                            return (printError(errString));
+                        }
+                    }
+                    numLines = atoi(numPtr);
+                    break;
+                default:
+                    strcat(errString, argv[i]);
+                    strcat(errString, "\'");
+                    return printError(errString);
+                    break;
+            }
+        }
+
+        while (i <= lim)
+        {
+            printf("Reading %s...\n", argv[i]);
+            FILE *filename = fopen(argv[i++], "r");
+            if (filename != NULL)
+            {
+                int numLines = atoi(argv[1]);
+                int numFileLines = 0;
+
+                while (fgets(line, MAXLINE, filename))
+                    numFileLines++;
+                numLines = numLines < numFileLines ? numLines : numFileLines;
+                rewind(filename);
+                while (fgets(line, MAXLINE, filename))
+                {
+                    if (numFileLines <= numLines)
+                        printf("%s", line);
+                    numFileLines--;
+                }
+            } else {
+                printf("%s\n", strerror(errno));
+            }
+            printf("\n");
+        }
     }
-    while (--counter >= 0) {
-        printf("%d: %p\n", counter, lines[counter]);
+    else
+    {
+        printf("%s: Usage - tail <numlines> <options> <files>\n", argv[0]);
+        return -1;
     }
 
     return 0;
 }
 
-int fgetline(char *line, int max) {
+int printError(char *err) {
+    printf("%s\n", err);
+    return -1;
+}
+
+int printTail(char* fname) {
+
+}
+
+int fgetline(char *line, int max)
+{
     int c, i;
 
     i = 0;
-    while (--max > 0 && (c=getchar()) != EOF && c != '\n')
+    while (--max > 0 && (c = getchar()) != EOF && c != '\n')
         line[i++] = c;
     if (c == '\n')
         line[i++] = c;
@@ -34,19 +113,21 @@ int fgetline(char *line, int max) {
     return i;
 }
 
-void appendLine (char *lines[], char *line) {
-    char tmpLine[MAXLINE];
-    // Need to allocate memory for each char array
+int appendLine(char *lines[], char *line)
+{
+    char *tmpLine = malloc(MAXLINE);
     copy(tmpLine, line);
-    printf("Line: %p\n", tmpLine);
     lines[counter++] = tmpLine;
+
+    return 1;
 }
 
-void copy(char to[], char from[]) {
+void copy(char to[], char from[])
+{
     int i = 0;
-    printf("Copy initialized.\nTo: %p\nFrom: %p\n", to, from);
 
-    while ((to[i] = from[i]) != '\0') {
+    while ((to[i] = from[i]) != '\0')
+    {
         ++i;
     }
 }
