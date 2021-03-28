32aa32245ca9083559492241aa1722368134fc30
astyles10 <alexanderstyles10@gmail.com>
2021-03-28 16:01:16 +1100

    Finish tail.c

--- Chapter5/tail.c
+++ Chapter5/tail.c
@@ -1,143 +1,143 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <ctype.h>
+#include <limits.h>
 #define MAXLINE 1000
+#define MIN_ARGS 2
+#define FAIL -1
 
-int printError(char *err);
-int fgetline(char *line, int max);
-int appendLine(char *lines[], char *line);
-void copy(char to[], char from[]);
-int counter = 0;
+int printError(char *, ...);
+int printTail(char *fname, int numLines);
+int parseOptions(char *argv[], int argc, int *argItr, int *numLines);
+int parseNumLines(char *arg);
 
 int main(int argc, char *argv[])
 {
-    char line[MAXLINE];
-    char *lines[MAXLINE];
-    int len = 0;
-    int numLines = 0;
-    char errString[MAXLINE] = "Invalid option: \'";
+    int numLines = 5;
+    int isSuccess = 0;
 
-    if (argc >= 2)
+    if (argc >= MIN_ARGS)
     {
-        int lim = argc - 1;
+        int argItr = 1;
+        isSuccess = parseOptions(argv, argc, &argItr, &numLines);
+        if (isSuccess == FAIL)
+            return isSuccess;
 
-        // Get options, increment i for each option
-        int i = 1;
-        while (argv[i][0] == '-')
+        int numFiles = argc - 1;
+
+        while (argItr <= numFiles)
         {
-            int opt = argv[i][1];
-            int optItr = 2;
-            switch (opt)
-            {
-            case 'n':
-                // Check if anything follows -n
-                if (strlen(argv[i]) < 3)
-                {
-                    strcat(errString, argv[i]);
-                    strcat(errString, "\' - Number of lines required");
-                    return printError(errString);
-                }
-                char *numPtr = &argv[i][optItr];
-
-                // Check if following input is a number
-                int x;
-                for (x = 0; x < strlen(numPtr); x++)
-                {
-                    if (!isdigit(numPtr[x]))
-                    {
-                        strcat(errString, argv[i]);
-                        strcat(errString, "\' - Invalid number of lines: ");
-                        strcat(errString, numPtr);
-                        return (printError(errString));
-                    }
-                }
-                numLines = atoi(numPtr);
-                break;
-            default:
-                strcat(errString, argv[i]);
-                strcat(errString, "\'");
-                return printError(errString);
-                break;
-            }
-            i++;
-        }
-
-        while (i <= lim)
-        {
-            printf("Reading %s...\n", argv[i]);
-            FILE *filename = fopen(argv[i++], "r");
-            if (filename != NULL)
-            {
-                int numLines = atoi(argv[1]);
-                int numFileLines = 0;
-
-                while (fgets(line, MAXLINE, filename))
-                    numFileLines++;
-                numLines = numLines < numFileLines ? numLines : numFileLines;
-                rewind(filename);
-                while (fgets(line, MAXLINE, filename))
-                {
-                    if (numFileLines <= numLines)
-                        printf("%s", line);
-                    numFileLines--;
-                }
-            }
-            else
-            {
-                printf("%s\n", strerror(errno));
-            }
+            isSuccess = printTail(argv[argItr++], numLines);
             printf("\n");
         }
     }
     else
+        return printError("Usage - tail <numlines> <options> <files>\n", NULL);
+
+    return isSuccess;
+}
+
+int printError(char * errs, ...)
+{
+    va_list pvlist;
+    va_start(pvlist, errs);
+    char errStr[MAXLINE] = "tail: ";
+    char *tmp = errs;
+
+    do {
+        if (tmp != NULL)
+            strcat(errStr, tmp);
+        tmp = va_arg(pvlist, char*);
+    } while (tmp != NULL);
+
+    va_end(pvlist);
+    printf("%s\n", errStr);
+
+    return FAIL;
+}
+
+int printTail(char *fname, int numLines)
+{
+    printf("Reading %s...\n", fname);
+    FILE *filename = fopen(fname, "r");
+    if (filename != NULL)
     {
-        printf("%s: Usage - tail <numlines> <options> <files>\n", argv[0]);
-        return -1;
+        char line[MAXLINE];
+        int numFileLines = 0;
+
+        while (fgets(line, MAXLINE, filename))
+            numFileLines++;
+
+        int tmpNumLines = numLines < numFileLines ? numLines : numFileLines;
+        rewind(filename);
+        while (fgets(line, MAXLINE, filename))
+        {
+            if (numFileLines <= tmpNumLines)
+                printf("%s", line);
+            numFileLines--;
+        }
     }
-
-    return 0;
+    else
+    {
+        return printError("error: ", strerror(errno), NULL);
+    }
 }
 
-int printError(char *err)
+int parseOptions(char *argv[], int argc, int *argItr, int *numLines)
 {
-    printf("%s\n", err);
-    return -1;
-}
+    while (argv[*argItr][0] == '-')
+    {
+        char *arg = argv[*argItr];
+        int opt = arg[1];
 
-int printTail(char *fname)
-{
-}
+        switch (opt)
+        {
+        case 'n':
+            if (strlen(arg) < 3) {
+                (*argItr)++;
+                if (*argItr == (argc - 1))
+                {
+                    printf("is it this one?\n");
+                    return printError("Usage - tail <numlines> <options> <files>\n", NULL);
+                }
+                strcat(arg, argv[*argItr]);
+            }
+            *numLines = parseNumLines(arg);
 
-int fgetline(char *line, int max)
-{
-    int c, i;
-
-    i = 0;
-    while (--max > 0 && (c = getchar()) != EOF && c != '\n')
-        line[i++] = c;
-    if (c == '\n')
-        line[i++] = c;
-    line[i] = '\0';
-    return i;
-}
-
-int appendLine(char *lines[], char *line)
-{
-    char *tmpLine = malloc(MAXLINE);
-    copy(tmpLine, line);
-    lines[counter++] = tmpLine;
-
+            if (*numLines < 0)
+                return *numLines;
+            break;
+        default:
+            return printError("Invalid option: \'", arg, "\'", NULL);
+            break;
+        }
+        (*argItr)++;
+        if (*argItr == argc)
+            return printError("Usage - tail <numlines> <options> <files>\n", NULL);
+    }
     return 1;
 }
 
-void copy(char to[], char from[])
+// Parse int value following -n
+int parseNumLines(char* arg)
 {
-    int i = 0;
+    unsigned long long int numLines = 0;
+    const int nOptLines = 2;
 
-    while ((to[i] = from[i]) != '\0')
-    {
-        ++i;
-    }
+    char *numPtr = &arg[nOptLines];
+
+    // Check if following input is a number
+    int x;
+    for (x = 0; x < strlen(numPtr); x++)
+        if (!isdigit(numPtr[x]))
+            return printError("Invalid number of lines: ", numPtr, NULL);
+
+    numLines = atoll(numPtr);
+    if (!(numLines <= INT_MAX))
+        return printError("Too many lines: ", numPtr, NULL);
+
+    return (int)numLines;
 }
